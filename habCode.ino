3896fdc3714ac8dc248e36358f7668282fb1139a
Samuel Stephens <86428266+dustmodebros@users.noreply.github.com>
2024-10-02 15:08:38 +0100

    Update habCode.ino

--- habCode.ino
+++ habCode.ino
@@ -8,10 +8,10 @@
 #include <Wire.h>
 
 /*
-   This sample code demonstrates the normal use of a TinyGPSPlus (TinyGPSPlus) object.
-   It requires the use of SoftwareSerial, and assumes that you have a
+   This code requires the use of SoftwareSerial, and assumes that you have a
    9600-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
 */
+
 static const int RXPin = 3, TXPin = 4;
 static const uint32_t GPSBaud = 9600;
 
